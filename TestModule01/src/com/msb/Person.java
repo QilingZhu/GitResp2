748bea616c8f5279153f8acfb628c3ae888a90f6
zhuql <cheec_limp@163.com>
2023-08-18 17:26:01 +0800

    进行Person类数据的提交

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @author : zhuql
+ * @date: 2023/8/18 - 08 - 18 - 17:21
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Person {
+    private String name;
+}
